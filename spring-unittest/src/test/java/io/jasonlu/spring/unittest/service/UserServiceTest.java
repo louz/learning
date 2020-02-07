4e322157ab9ae0eacb453543d3973ad127c5111f
louz <lujieheng@gmail.com>
2020-02-07 14:38:19 +0800

    update annotation

--- spring-unittest/src/test/java/io/jasonlu/spring/unittest/service/UserServiceTest.java
+++ spring-unittest/src/test/java/io/jasonlu/spring/unittest/service/UserServiceTest.java
@@ -2,16 +2,12 @@ package io.jasonlu.spring.unittest.service;
 
 import io.jasonlu.spring.unittest.Config;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {Config.class})  // 使用java代码配置时的方式
-// @ContextConfiguration(locations = {"classpath:spring.xml"}) 或 @ContextConfiguration("classpath:spring.xml")  使用xml配置文件时的使用方式
+@SpringJUnitConfig(Config.class)  // 使用java代码配置时的方式
 public class UserServiceTest {
 
     @Autowired
