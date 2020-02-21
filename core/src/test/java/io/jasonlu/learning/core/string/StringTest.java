35a9313309b758f2d5f8f0506262f2420e609d45
louz <lujieheng@gmail.com>
2020-02-21 09:54:34 +0800

    add string replace/replaceAll demo

--- core/src/test/java/io/jasonlu/learning/core/string/StringTest.java
+++ core/src/test/java/io/jasonlu/learning/core/string/StringTest.java
@@ -8,4 +8,15 @@ public class StringTest {
     public void testFormat() {
         System.out.println(String.format("%1$10s", "."));
     }
+
+    @Test
+    public void testReplaceAll() {
+        System.out.println("\\\"");
+
+        // replacAll的两个参数都是正则表达式，正则表达式中 \\\\ 代表一个 \
+        System.out.println("\\\"".replaceAll("\\\\", "\\\\\\\\").replaceAll("\"", "\\\\\""));   // 输出： \\\"
+
+        // replace的2个参数都是字符串，字符串中 \\ 代表一个 \
+        System.out.println("\\\"".replace("\\", "\\\\").replace("\"", "\\\""));                 // 输出： \\\"
+    }
 }
