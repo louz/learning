0bb40636c8603677b54f15ba41e6781b9881ce92
louz <lujieheng@gmail.com>
2020-02-21 09:55:21 +0800

    add replace

--- groovy-demo/src/hello.groovy
+++ groovy-demo/src/hello.groovy
@@ -1,3 +1,4 @@
+import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 
 def jsonSlurper = new JsonSlurper()
@@ -6,3 +7,12 @@ def object = jsonSlurper.parseText('{ "name": "John Doe", "address": {"street": 
 assert object instanceof Map
 assert object.name == 'John Doe'
 println(object.address.street)
+println(object.toString())      //  这个输出 [address:[house:112, street:1st road], name:John Doe]
+
+def json = JsonOutput.toJson(object)
+println(json)                   //  这个输出 {"address":{"house":112,"street":"1st road"},"name":"John Doe"}
+
+println(json.replaceAll('"', '\\\\"'))
+
+println("\\\", \"".replaceAll("\\\\", "\\\\\\\\").replaceAll("\"", "\\\\\""))
+println("\\")
